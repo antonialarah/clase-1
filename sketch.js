fcae1fa4e6887a3b3dd18f34d384a316e053b029
antonia lara <antonialarahern@gmail.com>
2025-08-25 21:26:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,20 @@
 var d;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
-  background(235,50,222);
+createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+  background(37,213,45);
 }
 
 function draw() {
-  d=random(10,60)
+  d= random(10,60)
   
-  colorMode(HSB,360,100,100);
-  fill(random(0,360),100,100);
-  ellipse(mouseX,mouseY,d,d);
+colorMode(HSB,360,100,100);
+fill(random(0,360),100,100);
+ellipse(mouseX,mouseY,d,d);
 }
-
 function keyPressed() {
   if (key === 'p') {
     save('dibujo.jpg');
-      }
+  }
 }
+ 
